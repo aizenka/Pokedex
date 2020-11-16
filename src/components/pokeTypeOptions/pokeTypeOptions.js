2b7b4ce8b985eb7ee8706c932f7bec5a27399d30
Andrew <jadedynastymail.ru@mail.ru>
2020-11-16 13:13:10 +0300

    implemented  poke type options

---
+++ src/components/pokeTypeOptions/pokeTypeOptions.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react'
+import { observer } from 'mobx-react'
+import './pokeTypeOptions.scss'
+
+const PokeTypeOptions = observer(({ store }) => {
+  useEffect(() => {
+    store.getAllTypes().then((data) => {
+      store.setPokeTypesOptions(data)
+    })
+  }, [store])
+
+  const getPokeOptions = (arr) => {
+    if (!arr) {
+      return
+    }
+    if (store.pokeTypeId === 0) {
+      return
+    }
+    return arr.map((optionItem, i) => {
+      const { id, name } = optionItem
+      return (
+        <option value={id.slice(1)} key={i}>
+          {name}
+        </option>
+      )
+    })
+  }
+
+  const opt = getPokeOptions(store.pokeTypesOptions)
+
+  return (
+    <div>
+      <span>Type: </span>
+      <select onChange={(e) => store.onTypeSelected(e.target.value)}>
+        {opt}
+      </select>
+    </div>
+  )
+})
+
+export default PokeTypeOptions
